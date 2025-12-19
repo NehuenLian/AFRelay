ddfaa9d7a4a346be52405fbfeea85b77ee9c3d58
Nehuen Lian <nehuenn.lian@gmail.com>
2025-12-18 22:34:05 -0300

    make docstring and names clearer

--- service/utils/convert_to_dict.py
+++ service/utils/convert_to_dict.py
@@ -3,18 +3,19 @@ from zeep.helpers import serialize_object
 from service.utils.logger import logger
 
 
-def convert_zeep_object_to_dict(returned_cae: object) -> dict:
+def convert_zeep_object_to_dict(returned_xml: object) -> dict:
 
     """
-    Zeep usually returns an object of type '<class 'zeep.objects.FECAEResponse'>'.
-    To work with the CAE data, this object needs to be converted into a dictionary.
+    Zeep usually returns an object of type '<class 'zeep.objects.[service response]'>'.
+    To work with the returned data, this object needs to be converted into a dictionary.
     This function performs that conversion and saves the data as a JSON file.
     """
 
     # Convert to dict/OrderedDict
-    CAE_response = serialize_object(returned_cae)
+    serialized_xml = serialize_object(returned_xml)
     logger.debug("Zeep object converted to dict.")
-    return CAE_response
+
+    return serialized_xml
 
 
 def convert_pydantic_model_to_dict(sale_data: object) -> dict:
