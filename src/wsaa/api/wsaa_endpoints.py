179ffc9a12af9cb71506401e1b9acb2dd0c28297
Nehuen Lian <nehuenn.lian@gmail.com>
2026-04-04 04:49:08 -0300

    refactor: modify loginCmsResponse model and add APIErrorResponseModel to /wsaa/loginCms

--- src/wsaa/api/wsaa_endpoints.py
+++ src/wsaa/api/wsaa_endpoints.py
@@ -1,18 +1,19 @@
 from fastapi import APIRouter, Depends
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 from src.shared.api.jwt_validator import verify_token
+from src.shared.api.response_models.errors import APIErrorResponseModel
 from src.shared.utils.logger import logger
 from src.wsaa.controllers.request_access_token_controller import \
     generate_afip_access_token
 
 
 class loginCmsResponse(BaseModel):
-    status: str = Field(..., json_schema_extra={"example": "success"})
+    status: str
 
 router = APIRouter()
 
-@router.post("/wsaa/loginCms", response_model=loginCmsResponse)
+@router.post("/wsaa/loginCms", response_model=loginCmsResponse | APIErrorResponseModel)
 async def renew_access_token(jwt = Depends(verify_token)) -> dict:
     
     logger.info("Received request to generate access token at /wsaa/loginCms")
