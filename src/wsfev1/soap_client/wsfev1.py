ba021ad1b835db69002bb6e16c7b5de7c4681277
Nehuen Lian <nehuenn.lian@gmail.com>
2026-04-17 05:23:10 -0300

    refactor: modify wsfev1 client to use httpx instead of zeep
    - remove health check logic
    - remove get_raw_xml_response function

--- src/wsfev1/soap_client/wsfev1.py
+++ src/wsfev1/soap_client/wsfev1.py
@@ -2,94 +2,54 @@ import logging
 from builtins import ConnectionResetError
 
 import httpx
-from lxml import etree
 from tenacity import (before_sleep_log, retry, retry_if_exception_type,
                       stop_after_attempt, wait_fixed)
-from zeep.exceptions import Fault, TransportError, XMLSyntaxError
-from zeep.helpers import serialize_object
 
-from src.shared.utils.format_error import build_error_response
 from src.shared.utils.logger import logger
+from src.shared.utils.response_management import (build_error_response,
+                                                  clear_response)
 from src.wsfev1.soap_client.client_manager import WSFEClientManager
-from src.wsfev1.soap_client.wsdl.wsdl_manager import get_wsfe_wsdl
+from src.wsfev1.soap_client.url_manager import get_wsfe_url
 
+
+def consult_afip_wsfe():
+    pass
 
 @retry(
-        retry=retry_if_exception_type(( ConnectionResetError, httpx.ConnectError, TransportError )),
+        retry=retry_if_exception_type(( ConnectionResetError, httpx.ConnectError )),
         stop=stop_after_attempt(3),
         wait=wait_fixed(0.5),
         before_sleep=before_sleep_log(logger, logging.WARNING),
     )
-async def consult_afip_wsfe(make_request, METHOD: str) -> dict:
-        
+async def consult_afip_wsfev1(xml_string, METHOD: str,  client=None) -> dict:
+
+    if client is None:
+        manager = WSFEClientManager()
+        client = manager.get_client()
+
+    url = get_wsfe_url()
+    headers = {
+        'Content-Type': 'text/xml; charset=utf-8',
+        'SOAPAction': f"http://ar.gov.afip.dif.FEV1/{METHOD}"
+    }
+    print(f"xml_string: {xml_string}")
     try:
-        afip_response = await make_request()
-
-        # get_raw_xml_response(METHOD) # Uncomment to log/save raw XML responses from the server
-
-        # Zeep returns an object of type '<class 'zeep.objects.[service response]'>'.
-        # To work with the returned data, this object needs to be converted into a dictionary using serialize_object().
-        afip_response = serialize_object(afip_response)
-
+        response = await client.post(url, content=xml_string, headers=headers)
+        print(f"response: {response}")
+        response.raise_for_status()
+        cleaned_response = clear_response(response)
+        print(f"cleaned_response: {cleaned_response}")
         return {
-                "status" : "success",
-                "response" : afip_response
-                }
-    
-    except (httpx.ConnectError, httpx.TimeoutException) as e:
-        return build_error_response(METHOD, "Network error", str(e))
-    
-    except TransportError as e:
+            "status" : "success",
+            "response" : cleaned_response
+        }
+
+    except httpx.HTTPStatusError as e:
         return build_error_response(METHOD, "HTTP Error", str(e))
 
-    except Fault as e:
-        # SOAP Faults here are not caused by user input.
-        # Zeep owns the XML generation, so any structural or datatype issue leading to a
-        # SOAP Fault originates from Zeep or the remote service, not from this layer.
-        
-        logger.debug(f"SOAP FAULT in {METHOD}: {e}")
-        return build_error_response(METHOD, "SOAPFault", str(e))
-    
-    except XMLSyntaxError as e:
-        return build_error_response(METHOD, "Invalid AFIP response", str(e))
+    except (httpx.ConnectError, httpx.TimeoutException) as e:
+        return build_error_response(METHOD, "Network error", str(e))
 
     except Exception as e:
         logger.error(f"General exception in {METHOD}: {e}")
         return build_error_response(METHOD, "unknown", str(e))
-    
-
-# Utils ================================
-def get_raw_xml_response(METHOD):
-    manager = WSFEClientManager(afip_wsdl)
-    xml_response = manager.history.last_received
-
-    raw_xml = etree.tostring(
-        xml_response["envelope"],
-        pretty_print=True,
-        encoding="unicode"
-    )
-
-    with open(f"docs/response_examples/{METHOD}Response.xml", "w") as f:
-        f.write(raw_xml)
-# ======================================
-
-# ===================
-# == HEALTH CHECK ===
-# ===================
-
-afip_wsdl = get_wsfe_wsdl()
-
-async def wsfe_dummy():
-    """
-    WSFE health cheack
-    """
-    logger.info(f"Consulting WSFE dummy method (health check)...")
-    manager = WSFEClientManager(afip_wsdl)
-    client = manager.get_client()
-    try:
-        health_info = await client.service.FEDummy()
-        return health_info
-
-    except Exception as e:
-        logger.error(f"General exception in wsfe_dummy: {e}")
-        return health_info
