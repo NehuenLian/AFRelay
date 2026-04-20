1d665a93a90014879a2b8df6d9c4e0d9faf3ffc2
Nehuen Lian <nehuenn.lian@gmail.com>
2026-04-20 04:37:52 -0300

    refactor: simplify WSFEClientManager singleton initialization and cleanup

--- src/wsfev1/soap_client/client_manager.py
+++ src/wsfev1/soap_client/client_manager.py
@@ -1,30 +1,24 @@
-
 import httpx
 
 
 class WSFEClientManager:
     _instance = None
-    _client = None
 
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
+            cls._instance._client = httpx.AsyncClient()
         return cls._instance
 
-    def __init__(self):
-        if self.__class__._client is None:
-            self.__class__._client = httpx.AsyncClient()
-
-    def get_client(self): 
-        return self.__class__._client
+    def get_client(self):
+        if self._client:
+            return self._client
     
     @classmethod
     def reset_singleton(cls):
         cls._instance = None
-        cls._client = None
 
     async def close(self) -> None:
-        if self.__class__._client:
-            await self.__class__._client.aclose()
-            self.__class__._instance = None
-            self.__class__._client = None
+        if self._client:
+            await self._client.aclose()
+            self.reset_singleton()
